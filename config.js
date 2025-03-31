afcecf1c9bf5b1f870084b9fed9615fa99cf7ac8
Dimarkos <104870419+Dimarkos21@users.noreply.github.com>
2025-03-31 14:00:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-? 'http://localhost:3003'
-: 'http://localhost:3003';
+? 'https://proyecto2-0-q74o.onrender.com'
+: "https://proyecto2-0-q74o.onrender.com";
 
 module.exports = { PAGE_URL }
